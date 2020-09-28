4f64cd74e68abe2c51d477365c8ba3280d1e79ef
joshleveck <63944775+joshleveck@users.noreply.github.com>
2020-09-28 16:20:15 -0400

    Add files via upload

---
+++ week2 - ciper.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[]) {
+
+    //check if they put in 26 char
+    if (strlen(argv[1]) == 26) {
+        for (int i = 0; i < strlen(argv[1]); i++) {
+            if (isalpha(argv[1][i])) {
+
+            }
+            else {
+                printf("Please include 26 characters after ./cipher");
+                return 1;
+            }
+        }
+
+        //get the text they want
+        string s = get_string ("Normal:");
+        printf("Ciper:");
+
+        for (int i = 0; i < strlen(s);){
+
+            //to keep the letter the same case check what case they are in, if not a letter print it
+            if (islower(s[i])) {
+                int l = s[i] - 97;
+                printf("%c", tolower(argv[1][l]));
+            }
+            else if (isupper(s[i])) {
+                int l = tolower(s[i]) - 97;
+                printf("%c", toupper(argv[1][l]));
+            }
+
+            else {
+                printf("%c", s[i]);
+            }
+            i++;
+        }
+        printf("\n");
+    }
+
+    //if its the wrong length return this
+    else {
+        printf("Please include 26 characters after ./cipher");
+        return 1;
+    }
+}
